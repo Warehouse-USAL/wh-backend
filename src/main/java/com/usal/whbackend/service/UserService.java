e30a8dc2c5155f0cc433ab15fb44514fcad548a1
maitena-perez <maitenaabril.perez@usal.edu.ar>
2026-06-07 21:02:13 +0000

    add self-service password change endpoint

--- src/main/java/com/usal/whbackend/service/UserService.java
+++ src/main/java/com/usal/whbackend/service/UserService.java
@@ -78,4 +78,20 @@ public class UserService {
     user.setPasswordHash(passwordEncoder.encode(newPassword));
     userRepository.save(user);
   }
+
+  public void changeMyPassword(String email, com.usal.whbackend.api.user.ChangePasswordRequest request) {
+    User user = userRepository.findByEmail(email)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "USER_NOT_FOUND"));
+
+    if (!passwordEncoder.matches(request.currentPassword(), user.getPasswordHash())) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "WRONG_CURRENT_PASSWORD");
+    }
+
+    if (passwordEncoder.matches(request.newPassword(), user.getPasswordHash())) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "SAME_PASSWORD");
+    }
+
+    user.setPasswordHash(passwordEncoder.encode(request.newPassword()));
+    userRepository.save(user);
+}
 }
